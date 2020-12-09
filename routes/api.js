e900124960475923e13034e42879fd59c267d93b
pmayur <thisispatil@gmail.com>
2020-12-10 00:09:50 +0530

    add: helmet to the application

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,17 @@
-'use strict';
+"use strict";
+const helmet = require("helmet");
 
 module.exports = function (app) {
+    app.use(
+        helmet.contentSecurityPolicy({
+            directives: {
+                defaultSrc: ["'self'"],
+            },
+        })
+    );
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    app.route("/api/stock-prices").get(function (req, res) {
+
+
     });
-    
 };
