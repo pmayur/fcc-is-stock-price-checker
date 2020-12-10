e362375b6463cdec18b193b075f0c913957b1aba
pmayur <thisispatil@gmail.com>
2020-12-10 20:16:01 +0530

    add: tests for viewing same stock:
    - same stock without like
    - same stock with like
    - same stock with like twice
    - invalid stock

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,127 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    test('Viewing one stock', function(done) {
+
+        let stock = 'goog';
+        let like = 'false';
+
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock, like })
+            .end( function(err, res) {
+                let data = res.body.stockData;
+
+                assert.equal(res.status, 200)
+                assert.exists(res.body.stockData)
+                assert.notExists(err)
+                assert.exists(data.stock)
+                assert.exists(data.price)
+                assert.exists(data.likes)
+                assert.equal(data.stock, stock.toUpperCase())
+                assert.isNumber(data.price)
+                assert.equal(data.likes, 0)
+                done()
+
+            })
+    })
+
+    test('Viewing one stock and liking it', function(done) {
+
+        let stock = 'goog';
+        let like = 'true';
+
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock, like })
+            .end( function(err, res) {
+                let data = res.body.stockData;
+
+                assert.equal(res.status, 200)
+                assert.exists(res.body.stockData)
+                assert.notExists(err)
+                assert.exists(data.stock)
+                assert.exists(data.price)
+                assert.exists(data.likes)
+                assert.equal(data.stock, stock.toUpperCase())
+                assert.isNumber(data.price)
+                assert.equal(data.likes, 1)
+                done()
+
+            })
+    })
+
+    test('Viewing the same stock and liking it again', function(done) {
+
+        let stock = 'goog';
+        let like = 'true';
+
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock, like })
+            .end( function(err, res) {
+                let data = res.body.stockData;
+
+                assert.equal(res.status, 200)
+                assert.exists(res.body.stockData)
+                assert.notExists(err)
+                assert.exists(data.stock)
+                assert.exists(data.price)
+                assert.exists(data.likes)
+                assert.equal(data.stock, stock.toUpperCase())
+                assert.isNumber(data.price)
+                assert.equal(data.likes, 1)
+                done()
+
+            })
+    })
+
+    test('Viewing another stock', function(done) {
+
+        let stock = 'msft';
+        let like = 'false';
+
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock, like })
+            .end( function(err, res) {
+                let data = res.body.stockData;
+
+                assert.equal(res.status, 200)
+                assert.exists(res.body.stockData)
+                assert.notExists(err)
+                assert.exists(data.stock)
+                assert.exists(data.price)
+                assert.exists(data.likes)
+                assert.equal(data.stock, stock.toUpperCase())
+                assert.isNumber(data.price)
+                assert.equal(data.likes, 0)
+                done()
+
+            })
+    })
+
+    test('Viewing invalid stock', function(done) {
+
+        let stock = 'fasfas';
+        let like = 'false';
+
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock, like })
+            .end( function(err, res) {
+                let data = res.body.stockData;
+
+                assert.equal(res.status, 200)
+                assert.exists(res.body.stockData)
+                assert.notExists(err)
+                assert.notExists(data.stock)
+                assert.notExists(data.price)
+                assert.exists(data.likes)
+                assert.equal(data.likes, 0)
+                done()
+
+            })
+    })
 
 });
